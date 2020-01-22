833a9d4e8bc89db6749c4b5c390b4399f7309081
Adil Suleman <38271146+adiljs@users.noreply.github.com>
2020-01-22 20:34:10 +0000

    Wednesday Done

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,82 @@
+﻿using LibraryApi.Domain;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LibraryApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryApi.Controllers
+{
+    public class BooksController : Controller
+    {
+        LibraryDataContext Context;
+
+        public BooksController(LibraryDataContext context)
+        {
+            Context = context;
+        }
+
+        IQueryable<Book> GetBooksInInventory()
+        {
+            return Context.Books
+                .Where(b => b.InInventory);
+        }
+
+
+        [HttpGet("/books/{id:int}")]
+        public async Task<IActionResult> GetBookById(int id)
+        {
+            var response = await GetBooksInInventory()
+                .Where(b => b.Id == id)
+                .Select(b => new GetBookDetailResponse
+                {
+                    Id = b.Id,
+                    Title = b.Title,
+                    Author = b.Author,
+                    Genre = b.Genre,
+                    NumberOfPages = b.NumberOfPages
+                }).SingleOrDefaultAsync();
+
+            if(response == null)
+            {
+                return NotFound("No book with that Id!");
+
+            }
+            else
+            {
+                return Ok(response);
+            }
+        }
+        
+        [HttpGet("/books")]
+        public async Task<IActionResult> GetAllBooks([FromQuery] string genre = "all")
+        {
+            var books = GetBooksInInventory();
+
+            if (genre != "all")
+            {
+                books = books.Where(b => b.Genre == genre);
+            }
+
+
+            var booksListItems = await books.Select(b => new BookSummaryItem
+            {
+                Id = b.Id,
+                Title = b.Title,
+                Author = b.Author,
+                Genre = b.Genre
+            }).ToListAsync();
+
+            var response = new GetBooksResponse
+            {
+                Data = booksListItems,
+                Genre = genre,
+                Count = booksListItems.Count()
+            };
+
+            return Ok(response); // for right now.
+        }
+    }
+}
